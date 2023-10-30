a528d9693916c68ec44f4520aecbf43d0df7891b
Simon Andrews <simon@proteo.me.uk>
2023-10-30 11:48:25 +0000

    Got image reading working

--- uk/ac/babraham/Beatbox/ImageLoader.java
+++ uk/ac/babraham/Beatbox/ImageLoader.java
@@ -5,10 +5,44 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class ImageLoader {
+import loci.formats.FormatTools;
+import loci.formats.ImageReader;
 
-	public ImageLoader(File baseFolder) {
+
+public class ImageLoader {
+	
+	private PixelMatrix pixeldata;
+
+	public ImageLoader(File input) {
+		if (input.isDirectory()) {
+			LoadTextFiles(input);
+		}
+		else {
+			LoadImage(input);
+		}
 		
+	}
+	
+	private void LoadImage(File file)  {
+		
+		try {
+			ImageReader imageReader = new ImageReader();
+			imageReader.setId(file.toString());
+			System.out.println("Images "+imageReader.getImageCount());
+			System.out.println("Pixeltype "+imageReader.getPixelType());
+			System.out.println("BytesPerPixel "+FormatTools.getBytesPerPixel(imageReader.getPixelType()));
+			
+			
+			imageReader.close();
+		}
+		catch (Exception ioe) {
+			ioe.printStackTrace();
+		}
+		
+		
+	}
+		
+	private void LoadTextFiles (File baseFolder) {
 		File [] files = baseFolder.listFiles(new FileFilter() {
 			
 			@Override
@@ -42,7 +76,7 @@ public class ImageLoader {
 			System.exit(1);
 		}
 		
-		PixelMatrix pixeldata = new PixelMatrix(dimensions[0], dimensions[1], arrayImages.length);
+		pixeldata = new PixelMatrix(dimensions[0], dimensions[1], arrayImages.length);
 
 		try {
 			for (int i=0;i<arrayImages.length;i++) {
@@ -60,6 +94,7 @@ public class ImageLoader {
 	
 	
 	public static void main(String [] args) {
-		new ImageLoader(new File("E:/Illumina Analysis/Stephen Cranwell/Periodicity Analysis/Data Parsing Test/text_image_analysis_test/"));
+//		new ImageLoader(new File("E:/Illumina Analysis/Stephen Cranwell/Periodicity Analysis/Data Parsing Test/text_image_analysis_test/"));
+		new ImageLoader(new File("E:/Illumina Analysis/Stephen Cranwell/Periodicity Analysis/Data Parsing Test/C4_ROI2_IF.nd2"));
 	}
 }
